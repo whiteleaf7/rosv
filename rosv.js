ca9fb685c299c6be795e01a5e355dc198d790314
whiteleaf <2nd.leaf@gmail.com>
2011-05-15 23:31:22 +0900

    add license, wrote README

--- rosv.js
+++ rosv.js
@@ -1,5 +1,5 @@
 ﻿//
-// RO SerVer checker by sasa
+// RO SerVer checker by whiteleaf
 //
 // つかいかた
 // rosv>help 参照
@@ -9,6 +9,28 @@
 // 2011/03/12 Ver.1.2 コマンドが間違っているときに有効かどうかチェックするようにした
 // 2011/03/09 Ver.1.1 表示形式変更、Array#include を使うように修正
 // 2011/03/07 Ver.1.0
+//
+// The MIT License
+//
+// Copyright (c) 2011 whiteleaf
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
 var Rosv = {
